fbea7402d7d4ed02c7b48929221938e8249d683d
nazfrancesredleene <francesredleenepnaz@gmail.com>
2023-02-03 10:49:16 +0800

    modified:   package-lock.json modified:   src/components/Acomplishments/Acomplishments.js modified:   src/components/Footer/Footer.js modified:   src/components/Header/Header.js modified:   src/components/Header/HeaderStyles.js modified:   src/components/Hero/Hero.js modified:   src/components/Projects/Projects.js modified:   src/components/Technologies/Technologies.js modified:   src/components/TimeLine/TimeLine.js modified:   src/pages/index.js modified:   yarn.lock

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,34 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:+44777777777">+447777777</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>email</LinkTitle>
+          <LinkItem href="myemail@gmail.com">myemail@gmail.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>One Project at a time</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href = "https://github.com">
+            <AiFillGithub size = "3rem"/>
+          </SocialIcons>
+          <SocialIcons href = "https://linkedin.com">
+            <AiFillLinkedin size = "3rem"/>
+          </SocialIcons>
+          <SocialIcons href = "https://instagram.com">
+            <AiFillInstagram size = "3rem"/>
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -4,9 +4,43 @@ import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id = "tech">
+    <SectionDivider/>
+    <br/>
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've worked with range of technologies in the web and mobile development world. From Design, Front-end to Back-end
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size = "3rem"/>
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+        </ListContainer>
+        <ListParagraph>
+          Experience with <br/> React.js
+        </ListParagraph>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size = "3rem"/>
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+        </ListContainer>
+        <ListParagraph>
+          Experience with <br/> Node and Databases
+        </ListParagraph>
+      </ListItem>
+      <ListItem>
+        <DiZend size = "3rem"/>
+        <ListContainer>
+          <ListTitle>UI/UX</ListTitle>
+        </ListContainer>
+        <ListParagraph>
+          Experience with <br/> Figma
+        </ListParagraph>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
